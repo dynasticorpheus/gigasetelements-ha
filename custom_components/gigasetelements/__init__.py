dynasticorpheus/gigasetelements-ha

commit d7ab524ede9f3d5cc72141fc19eecde08d733231

update also during triggered state

custom_components/gigasetelements/__init__.py
@@ -163,13 +163,12 @@ class GigasetelementsClientAPI:
 
         if not refresh:
             return self._state
-        elif self._state == STATE_ALARM_TRIGGERED and self._health == STATE_HEALTH_RED:
-            return self._state
         else:
             if time.time() - self._last_authenticated > AUTH_GSE_EXPIRE:
                 self._last_authenticated = self._do_authorisation()
             self._basestation_data = self._do_request("GET", URL_GSE_API + "/v1/me/basestations")
             self._elements_data = self._do_request("GET", URL_GSE_API + "/v2/me/elements")
+            self._health_data = self._do_request("GET", URL_GSE_API + "/v3/me/health")
             self._event_data = self._do_request(
                 "GET", URL_GSE_API + "/v2/me/events?from_ts=" + self._last_event
             )
@@ -336,7 +335,6 @@ class GigasetelementsClientAPI:
 
         sensor_attributes = {}
 
-        self._health_data = self._do_request("GET", URL_GSE_API + "/v3/me/health")
         try:
             if self._health_data.json()["systemHealth"] == "green":
                 self._health = STATE_HEALTH_GREEN

commit 5271e4f1c6e2e8706af9a72df160ca0e20f4000d

fix regression alarm panel status introduced with d7ab524

custom_components/gigasetelements/__init__.py
@@ -183,6 +183,13 @@ class GigasetelementsClientAPI:
             )
         ]
 
+        try:
+            if self._health_data.json()["statusMsgId"] in ["alarm.user", "system_intrusion"]:
+                self._state = STATE_ALARM_TRIGGERED
+                _LOGGER.debug("Alarm trigger state: %s", self._health_data.json()["statusMsgId"])
+        except (KeyError, ValueError):
+            pass
+
         if self._target_state == 0:
             self._target_state = self._state
 
@@ -190,16 +197,18 @@ class GigasetelementsClientAPI:
 
         diff = time.time() - self._pending_time
 
-        if self._state == self._target_state:
-            self._pending_time = time.time()
-            return self._state
-        elif diff > PENDING_STATE_THRESHOLD:
-            self._target_state = self._state
-            _LOGGER.warning(
-                "Pending time threshold exceeded, sync alarm target state: %s", self._target_state
-            )
-        else:
-            return STATE_ALARM_PENDING
+        if self._state != STATE_ALARM_TRIGGERED:
+            if self._state == self._target_state:
+                self._pending_time = time.time()
+                return self._state
+            elif diff > PENDING_STATE_THRESHOLD:
+                self._target_state = self._state
+                _LOGGER.warning(
+                    "Pending time threshold exceeded, sync alarm target state: %s",
+                    self._target_state,
+                )
+            else:
+                return STATE_ALARM_PENDING
 
         return self._state
 
@@ -365,11 +374,6 @@ class GigasetelementsClientAPI:
                 self._health = STATE_HEALTH_ORANGE
             elif self._health_data.json()["systemHealth"] == "red":
                 self._health = STATE_HEALTH_RED
-                if self._health_data.json()["statusMsgId"] in ["alarm.user", "system_intrusion"]:
-                    self._state = STATE_ALARM_TRIGGERED
-                    _LOGGER.debug(
-                        "Alarm trigger state: %s", self._health_data.json()["statusMsgId"]
-                    )
             else:
                 self._health = STATE_UNKNOWN
 

custom_components/gigasetelements/const.py
@@ -99,7 +99,7 @@ HEADER_GSE = {
     "user-agent": "Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201",
 }
 
-PENDING_STATE_THRESHOLD = 30
+PENDING_STATE_THRESHOLD = 10
 
 SENSOR_NAME = {
     "bs01": "base",